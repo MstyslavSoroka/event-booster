cdfebc79c09ac6f04c147cf85d00b624320cd004
Ivan Kishko1~ <oknavi779@gmail.com>
2025-03-22 11:07:05 +0100

    finish pagination

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import { renderPosts } from "./js/render.js";
-import { findPosts } from "./js/findPosts.js";
+import { renderPosts } from './js/render.js';
+import { findPosts } from './js/findPosts.js';
+import { pagination } from './js/pagination.js';
 
 const apikey = 'Z0jpFmdgR3VO2MAgupFCQ1c6yNovbVdi';
+const elementsPerPage = 5;
+let currentPage = 1;
 
-findPosts(apikey).then(posts => renderPosts(posts));
+async function loadPosts(currentPage) {
+  const posts = await findPosts(apikey);
+  const totalPages = Math.ceil(posts.length / elementsPerPage);
+  const startIndex = (currentPage - 1) * elementsPerPage;
+  const endIndex = currentPage * elementsPerPage;
+
+  renderPosts(posts.slice(startIndex, endIndex));
+  pagination(totalPages, currentPage, loadPosts);
+}
+
+loadPosts(currentPage);
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,22 +1,18 @@
-function pagination(totalPages, currentPage) {
+export function pagination(totalPages, currentPage, loadPosts) {
   const pags = document.getElementById('pags');
   pags.innerHTML = '';
 
   for (let i = 1; i <= totalPages; i++) {
-    if (i > 5 && i < totalPages) {
-      if (i === 6) {
-      }
-      continue;
-    }
     const page = document.createElement('span');
     page.innerText = i;
     page.classList.add('page');
     if (i === currentPage) {
       page.classList.add('active');
     }
-    page.onclick = () => pagination(totalPages, i);
+    page.onclick = () => {
+      currentPage = i;
+      loadPosts(currentPage);
+    };
     pags.appendChild(page);
   }
 }
-
-pagination(5, 1);
